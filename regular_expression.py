8d4429c958e16fb9da47f14d2c03f14054e48788
Steve <ziyaozhu@connect.hku.hk>
2022-01-28 04:48:42 +0800

    add exception handle

--- regular_expression.py
+++ regular_expression.py
@@ -2,33 +2,43 @@ import requests
 import re
 import os
 
-if __name__ == "__main__":
-    #if not os.path.exists('./re'):
-        #os.mkdir('./re')
+URL = 'https://zh.moegirl.org.cn/zh-tw/%E9%AD%94%E5%A5%B3%E4%B9%8B%E6%97%85'
+MAX_REATTEMPTS = 50
+TIMEOUT = 3
 
-    url = 'https://zh.moegirl.org.cn/zh-tw/%E9%AD%94%E5%A5%B3%E4%B9%8B%E6%97%85'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
-    }
-    page_text = requests.get(url=url, headers=headers).text
+if __name__ == "__main__":
+    if not os.path.exists('./re'):
+        os.mkdir('./re')
+
+    page_text = requests.get(URL).text
 
     ex = '<img.*?src="(.*?)".*?>'
     img_src_list = re.findall(ex, page_text, re.S)
+    total = len(img_src_list)
 
-    t=0
-    with open('./re.html', 'w', encoding='utf-8') as fp:
+    with open('./re.html', 'w') as fp:
         for src in img_src_list:
             fp.write(src+'\n')
-            t+=1
 
     count = 0
-    print('there are ',t, ' images'  )
+    print(f'There are {total} images')
     for src in img_src_list:
-        img_data = requests.get(url=src,headers=headers).content
+        print(src)
         img_name = src.split('/')[-1]
-        img_path = './re/'+img_name
-        with open(img_path, 'wb' ) as fp:
-            fp.write(img_data)
-            count +=1
-            print(img_name,' success!!!', count, ' / ', t)
-    print('finish!!')
+        img_path = './re/' + img_name
+        for i in range(MAX_REATTEMPTS):
+            try:
+                img_data = requests.get(url=src, timeout=TIMEOUT).content
+                with open(img_path, 'wb') as fp:
+                    fp.write(img_data)
+                count += 1
+                print(f"{img_name} succeed!!! {count}/{total}")
+                break
+
+            except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
+                pass
+
+            except requests.exceptions.MissingSchema:
+                break
+
+    print('finished!!')
